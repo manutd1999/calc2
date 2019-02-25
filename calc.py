ef546fb0b997d2994e745ae75fb6cb4c58079cfe
Man Utd <manutd1999ca@gmail.com>
2019-02-24 21:05:54 -0500

    Initial confirmed commit

---
+++ calc.py
@@ -0,0 +1,26 @@
+# import numpy
+import argparse
+
+parser = argparse.ArgumentParser()
+
+print('Testing')
+
+
+def add(x, y):
+    return x+y
+
+
+def subtract(x, y):
+    return x-y
+
+
+def multiply(x, y):
+    return x*y
+
+
+def divide(x, y):
+    return x/y
+
+
+def square(x):
+    pass
